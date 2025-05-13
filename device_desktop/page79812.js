1f0ea0ee914867c6d58bd56dd8ae082e3c78c558
suci080302 <Suciannisya67@gmail.com>
2025-05-13 14:37:02 +0700

    Add files via upload

---
+++ device_desktop/page79812.js
@@ -0,0 +1,64 @@
+DesktopResponsive={"1009":{
+"pageLayer":{"w":1009,"h":662,"bgColor":"#99c9bb","bgImage":"images/background.jpg","bgSize":"1009px 127px","bgRepeat":"repeat-y"}
+,
+"button40512":{"x":172,"y":5,"w":41.000000,"h":53.000000,"stylemods":[{"sel":"div.button40512Text","decl":" { position:fixed; left:2px; top:2px; width:36px; height:48px;}"},{"sel":"span.button40512Text","decl":" { display:table-cell; position:relative; width:36px; height:48px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAA1CAYAAADcdMIWAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBAQAAAIIg/69uSEAAAAAAAAAAADyoASIpAAGOWTcxAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAA1CAYAAADcdMIWAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBAQAAAIIg/69uSEAAAAAAAAAAADyoASIpAAGOWTcxAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAA1CAYAAADcdMIWAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBAQAAAIIg/69uSEAAAAAAAAAAADyoASIpAAGOWTcxAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAA1CAYAAADcdMIWAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBAQAAAIIg/69uSEAAAAAAAAAAADyoASIpAAGOWTcxAAAAAElFTkSuQmCC"  ,"fd": "images/home_norm.png" ,"fdO": "images/home_over.png" ,"fdD": "images/home_click.png" ,"fdDi": "images/home_norm.png" ,"p": "M 0.000000 0.000000 L 40.000000 0.000000 L 40.000000 52.000000 L 0.000000 52.000000 L 0.000000 0.000000 z","i":"images/button40512.png","irol":"images/button40512_over.png","ion":"images/button40512_down.png","idis":"images/button40512_disabled.png"}
+,
+"button40506":{"x":287,"y":2,"w":42.000000,"h":55.000000,"stylemods":[{"sel":"div.button40506Text","decl":" { position:fixed; left:2px; top:2px; width:37px; height:50px;}"},{"sel":"span.button40506Text","decl":" { display:table-cell; position:relative; width:37px; height:50px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAA3CAYAAAB6i9geAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBDQAAAMKg90/t7AEEAAAAAAAAAABwNSRPAAFgweazAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAA3CAYAAAB6i9geAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBDQAAAMKg90/t7AEEAAAAAAAAAABwNSRPAAFgweazAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAA3CAYAAAB6i9geAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBDQAAAMKg90/t7AEEAAAAAAAAAABwNSRPAAFgweazAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAA3CAYAAAB6i9geAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cEBDQAAAMKg90/t7AEEAAAAAAAAAABwNSRPAAFgweazAAAAAElFTkSuQmCC"  ,"fd": "images/exit_norm.png" ,"fdO": "images/exit_over.png" ,"fdD": "images/exit_click.png" ,"fdDi": "images/exit_norm.png" ,"p": "M 0.000000 0.000000 L 41.000000 0.000000 L 41.000000 54.000000 L 0.000000 54.000000 L 0.000000 0.000000 z","i":"images/button40506.png","irol":"images/button40506_over.png","ion":"images/button40506_down.png","idis":"images/button40506_disabled.png"}
+,
+"button40499":{"x":747,"y":9,"w":62.000000,"h":47.000000,"stylemods":[{"sel":"div.button40499Text","decl":" { position:fixed; left:2px; top:2px; width:57px; height:42px;}"},{"sel":"span.button40499Text","decl":" { display:table-cell; position:relative; width:57px; height:42px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":1  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC"  ,"fd": "images/back_norm.png" ,"fdO": "images/back_over.png" ,"fdD": "images/back_click.png" ,"fdDi": "images/back_norm.png" ,"p": "M 0.000000 0.000000 L 61.000000 0.000000 L 61.000000 46.000000 L 0.000000 46.000000 L 0.000000 0.000000 z","i":"images/button40499.png","irol":"images/button40499_over.png","ion":"images/button40499_down.png","idis":"images/button40499_disabled.png"}
+,
+"button40492":{"x":828,"y":9,"w":62.000000,"h":47.000000,"stylemods":[{"sel":"div.button40492Text","decl":" { position:fixed; left:2px; top:2px; width:57px; height:42px;}"},{"sel":"span.button40492Text","decl":" { display:table-cell; position:relative; width:57px; height:42px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":1  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAiSURBVGhD7cExAQAAAMKg9U9tCj8gAAAAAAAAAAAAAAB+ai23AAEbFHswAAAAAElFTkSuQmCC"  ,"fd": "images/next_norm.png" ,"fdO": "images/next_over.png" ,"fdD": "images/next_click.png" ,"fdDi": "images/next_norm.png" ,"p": "M 0.000000 0.000000 L 61.000000 0.000000 L 61.000000 46.000000 L 0.000000 46.000000 L 0.000000 0.000000 z","i":"images/button40492.png","irol":"images/button40492_over.png","ion":"images/button40492_down.png","idis":"images/button40492_disabled.png"}
+,
+"button142":{"x":232,"y":3,"w":45.000000,"h":55.000000,"stylemods":[{"sel":"div.button142Text","decl":" { position:fixed; left:2px; top:2px; width:40px; height:50px;}"},{"sel":"span.button142Text","decl":" { display:table-cell; position:relative; width:40px; height:50px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAA3CAYAAACYV8NnAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAhSURBVGhD7cExAQAAAMKg9U9tDQ8gAAAAAAAAAAAAeFQDJuMAAfzF8OQAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAA3CAYAAACYV8NnAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAhSURBVGhD7cExAQAAAMKg9U9tDQ8gAAAAAAAAAAAAeFQDJuMAAfzF8OQAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAA3CAYAAACYV8NnAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAhSURBVGhD7cExAQAAAMKg9U9tDQ8gAAAAAAAAAAAAeFQDJuMAAfzF8OQAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAA3CAYAAACYV8NnAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAhSURBVGhD7cExAQAAAMKg9U9tDQ8gAAAAAAAAAAAAeFQDJuMAAfzF8OQAAAAASUVORK5CYII="  ,"fd": "images/help_norm.png" ,"fdO": "images/help_over.png" ,"fdD": "images/help_click.png" ,"fdDi": "images/help_norm.png" ,"p": "M 0.000000 0.000000 L 44.000000 0.000000 L 44.000000 54.000000 L 0.000000 54.000000 L 0.000000 0.000000 z","i":"images/button142.png","irol":"images/button142_over.png","ion":"images/button142_down.png","idis":"images/button142_disabled.png"}
+,
+"audio26512":{"x":960,"y":5,"w":30,"h":30,"bOffBottom":0}
+,
+"image24363":{"x":4,"y":70,"w":73,"h":72,"bOffBottom":0,"i":"images/tombol_menu.png"}
+,
+"image40518":{"x":0,"y":0,"w":1010,"h":100,"bOffBottom":0,"i":"images/background_topcrop_image40518.png"}
+,
+"text40487":{"x":213,"y":14,"w":542,"h":28,"txtscale":100,"bOffBottom":0}
+,
+"image310":{"x":0,"y":71,"w":1009,"h":591,"bOffBottom":0,"i":"images/langit.png"}
+,
+"image2203":{"x":65,"y":-5,"w":62,"h":62,"bOffBottom":0,"i":"images/logo_upn_universitas_negeri_padang_png.png"}
+,
+"image2238":{"x":1,"y":0,"w":70,"h":50,"bOffBottom":0,"i":"images/tut_wuri_handayani_vector_logo.png"}
+,
+"image81549":{"x":0,"y":136,"w":1009,"h":528,"bOffBottom":0,"i":"images/bukit.png"}
+,
+"image81040":{"x":0,"y":101,"w":376,"h":345,"bOffBottom":0,"i":"images/tangan_pohon.png"}
+,
+"image82202":{"x":121,"y":321,"w":766,"h":363,"bOffBottom":0,"i":"images/image_3_59794.jpg"}
+,
+"image79835":{"x":276,"y":120,"w":454,"h":153,"bOffBottom":0,"i":"images/image_3_59794.jpg"}
+,
+"text82560":{"x":295,"y":159,"w":399,"h":88,"txtscale":100,"bOffBottom":0,"bltArr":null}
+,
+"text86755":{"x":147,"y":349,"w":399,"h":88,"txtscale":100,"bOffBottom":0,"bltArr":null}
+,
+"image87326":{"x":187,"y":382,"w":259,"h":146,"bOffBottom":0,"i":"images/image_11_87325.jpg"}
+,
+"image87489":{"x":539,"y":376,"w":271,"h":153,"bOffBottom":0,"i":"images/image_11001_87488.jpg"}
+,
+"text88203":{"x":269,"y":528,"w":80,"h":44,"txtscale":100,"bOffBottom":0}
+,
+"text88554":{"x":630,"y":529,"w":80,"h":62,"txtscale":100,"bOffBottom":0}
+,
+"text88681":{"x":147,"y":560,"w":399,"h":88,"txtscale":100,"bOffBottom":0,"bltArr":null}
+,
+"image90078":{"x":441,"y":66,"w":126,"h":84,"bOffBottom":0,"i":"images/papan_nama.png"}
+,
+"text82311":{"x":457,"y":102,"w":94,"h":37,"txtscale":100,"bOffBottom":0}
+,
+"image90332":{"x":441,"y":270,"w":126,"h":84,"bOffBottom":0,"i":"images/papan_nama.png"}
+,
+"text82495":{"x":457,"y":303,"w":94,"h":37,"txtscale":100,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/help_norm.png','images/help_click.png','images/help_over.png','images/langit.png','images/papan_nama.png','images/logo_upn_universitas_negeri_padang_png.png','images/tut_wuri_handayani_vector_logo.png','images/tombol_menu.png','images/next_norm.png','images/next_click.png','images/next_over.png','images/back_norm.png','images/back_click.png','images/back_over.png','images/exit_norm.png','images/exit_click.png','images/exit_over.png','images/home_norm.png','images/home_click.png','images/home_over.png','images/background.jpg','images/image_3_59794.jpg','images/bukit.png','images/tangan_pohon.png','images/image_11_87325.jpg','images/image_11001_87488.jpg','images/background_topcrop_image40518.png','images/background_top002crop_image40518.png','images/background_top001crop_image40518.png','images/background_top003crop_image40518.png','images/button40506.png','images/button40506_over.png','images/button40506_down.png','images/button40506_disabled.png','images/button40499.png','images/button40499_over.png','images/button40499_down.png','images/button40499_disabled.png','images/button40492.png','images/button40492_over.png','images/button40492_down.png','images/button40492_disabled.png','images/button142.png','images/button142_over.png','images/button142_down.png','images/button142_disabled.png','images/button40512.png','images/button40512_over.png','images/button40512_down.png','images/button40512_disabled.png','images/text82560ArrowBulletFont9.png','images/text86755ArrowBulletFont9.png','images/text88681ArrowBulletFont9.png']
+}}
